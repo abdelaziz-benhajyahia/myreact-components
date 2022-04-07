50cdb3e9b22aeedf36433ab598e0cd92ba7c2064
abdelaziz-benhajyahia <61843821+abdelaziz-benhajyahia@users.noreply.github.com>
2022-04-07 09:04:11 +0100

    Add files via upload

---
+++ src/Component/Profile/Address.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Address = () => {
+  return (
+        <div className='Address'>
+            <h3>2001 Street, Oslo, Norway</h3>
+        </div>
+  )
+}
+
+export default Address
---
+++ src/Component/Profile/ProfilePhoto.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ProfilePhoto = () => {
+    return (
+        <div className='profilePhoto'>
+        <img src='/profile_picture.jpg' alt='profilePhoto' />
+        </div>
+    )
+}
+
+export default ProfilePhoto
